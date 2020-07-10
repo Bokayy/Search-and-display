75f805007c8c7cdf3efb7cae5ef020efb49100c3
Mladen Panic <mladen.panic@profil-klett.hr>
2020-07-10 11:37:17 +0200

    IE suport

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ]
-}
+  ],
+  // Use this option if you need to transpile CommonJS modules using transpileDependencies 
+  // https://github.com/vuejs/vue-cli/issues/4773
+  // sourceType: 'unambiguous',
+};
